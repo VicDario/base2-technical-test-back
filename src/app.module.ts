7e297cf3dfead1ff18a1d4f422ca07e54ab04132
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-15 17:02:53 -0400

    feat: import all main modules to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { envs, validateConfig } from './config/env.config';
+import { RoutesModule } from './presentation/routes/routes.module';
+import { MongoDatabaseModule } from './data/mongo/mongo-database.module';
+import { RepositoriesModule } from './infrastructure/repositories/repositories.module';
+import { UseCasesModule } from './infrastructure/use-cases/use-cases.module';
+import { DatasourcesModule } from './infrastructure/datasources/datasources.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -10,6 +16,11 @@ import { envs, validateConfig } from './config/env.config';
       isGlobal: true,
       validationSchema: validateConfig,
     }),
+    MongoDatabaseModule,
+    RoutesModule,
+    RepositoriesModule,
+    UseCasesModule,
+    DatasourcesModule,
   ],
 })
 export class AppModule {}
