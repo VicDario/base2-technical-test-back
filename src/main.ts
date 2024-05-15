37cc3275cde37c90c2cdcd7ebb552900dc566f13
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-14 23:01:00 -0400

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   // Swagger configuration
   const config = new DocumentBuilder()
     .setTitle('Base2 Technical Test')
