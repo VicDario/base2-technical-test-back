e734931d736bf04ed5e6990b37c588351fa91a0d
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-15 18:46:52 -0400

    feat: add abstract category repository

---
+++ src/domain/repositories/category.repository.ts
@@ -0,0 +1,18 @@
+import { CategoryEntity } from '../entities/category.entity';
+
+export interface CategoriesResult {
+  total: number;
+  categories: CategoryEntity[];
+}
+
+export interface Pagination {
+  offset: number;
+  limit: number;
+}
+
+export abstract class CategoryRepository {
+  abstract getCategories(pagination: Pagination): Promise<CategoriesResult>;
+  abstract getCategoryById(id: string): Promise<CategoryEntity>;
+  abstract createCategory(category: CategoryEntity): Promise<CategoryEntity>;
+  abstract deleteCategory(id: string): Promise<CategoryEntity>;
+}
