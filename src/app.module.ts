c9d8df5f45a82b79ed651a4f16a4ba98071c9f4d
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-15 12:25:09 -0400

    chore: delete unnecesary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 
 import { envs, validateConfig } from './config/env.config';
@@ -13,7 +11,5 @@ import { envs, validateConfig } from './config/env.config';
       validationSchema: validateConfig,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
