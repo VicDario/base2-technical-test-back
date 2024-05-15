8e8efcbada6d29dd3016ab694ae922461856ca15
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-15 14:58:46 -0400

    feat: add mongo module

---
+++ src/data/mongo/mongo-database.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { ConfigType } from '@nestjs/config';
+import { envs } from '@/config/env.config';
+import { Product, ProductSchema } from './models/product.model';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      inject: [envs.KEY],
+      useFactory: async (configService: ConfigType<typeof envs>) => {
+        const { mongoUrl } = configService;
+        return {
+          uri: mongoUrl,
+        };
+      },
+    }),
+  ],
+  exports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+  ],
+})
+export class MongoDatabaseModule {}
