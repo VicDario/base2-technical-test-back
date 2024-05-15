d7d577f8a81b440cb6e49505cdb5a188b437c047
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-15 14:58:34 -0400

    feat: add product model for mongo database

---
+++ src/data/mongo/models/product.model.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ProductDocument = HydratedDocument<Product>;
+
+@Schema()
+export class Product {
+  @Prop()
+  name: string;
+
+  @Prop()
+  sku: string;
+
+  @Prop()
+  price: number;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  images: string[];
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
