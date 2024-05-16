6be9f2eceec2d21e33a5580ceaa987de1859f908
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-16 13:10:10 -0400

    feat: connect helmet

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(helmet());
   app.enableCors();
 
   app.setGlobalPrefix('api');
