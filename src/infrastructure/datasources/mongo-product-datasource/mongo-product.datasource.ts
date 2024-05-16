f7d3241b0f92dda1d86ee31235ea424e2456c049
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-15 22:31:12 -0400

    refactor: correct responses, apply filters and exceptions

--- src/infrastructure/datasources/mongo-product-datasource/mongo-product.datasource.ts
+++ src/infrastructure/datasources/mongo-product-datasource/mongo-product.datasource.ts
@@ -2,12 +2,14 @@ import { Product } from '@/data/mongo/models/product.model';
 import {
   FilterProduct,
   Pagination,
-  ProductDatasource,
-} from '@/domain/datasources/product.datasource';
+  ProductsResult,
+} from '@/domain/repositories/product.repository';
 import { ProductEntity } from '@/domain/entities/product.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { ProductDatasource } from '@/domain/datasources/product.datasource';
+import { SkuConflictException } from '@/infrastructure/exceptions/skuConflict.exception';
 
 @Injectable()
 export class MongoProductDatasource implements ProductDatasource {
@@ -18,36 +20,62 @@ export class MongoProductDatasource implements ProductDatasource {
   async getProducts(
     filter: FilterProduct = {},
     pagination: Pagination,
-  ): Promise<ProductEntity[]> {
-    const products = await this.productModel
-      .find(filter)
+  ): Promise<ProductsResult> {
+    const productFilter = {};
+    if (filter.name) productFilter['name'] = { $regex: filter.name, $options: 'i' };
+    if (filter.sku) productFilter['sku'] = { $regex: filter.sku, $options: 'i' };
+    if (filter.minPrice) productFilter['price'] = { $gte: filter.minPrice };
+    if (filter.maxPrice) productFilter['price'] = { ...productFilter['price'], $lte: filter.maxPrice };    
+
+    const query = this.productModel
+      .find(productFilter)
       .skip(pagination.offset)
       .limit(pagination.limit)
-      .exec();
-    return products.map(ProductEntity.fromObject);
+      .populate('category');
+    const totalProducts = query.clone().estimatedDocumentCount().exec();
+    const productsResult = await query.exec();
+    const products = productsResult.map(ProductEntity.fromObject);
+    console.log(productsResult, productFilter);
+    return {
+      products,
+      total: await totalProducts,
+    };
   }
 
   async getProductById(id: string): Promise<ProductEntity> {
-    const product = await this.productModel.findById(id).exec();
-    return ProductEntity.fromObject(product.toObject());
+    const product = await this.productModel
+      .findById(id)
+      .populate('category')
+      .exec();
+    return ProductEntity.fromObject(product);
   }
   async createProduct(product: ProductEntity): Promise<ProductEntity> {
-    const newProduct = await this.productModel.create(product);
-    return ProductEntity.fromObject(newProduct.toObject());
+    try {
+      const newProduct = await this.productModel.create(product);
+      return ProductEntity.fromObject(newProduct);
+    } catch (error) {
+      if (error.code === 11000)
+        throw new SkuConflictException(product.sku);
+    }
   }
 
   async updateProduct(
     id: string,
     product: ProductEntity,
   ): Promise<ProductEntity> {
-    const updatedProduct = await this.productModel
-      .findOneAndUpdate({ id }, product, { new: true })
-      .exec();
-    return ProductEntity.fromObject(updatedProduct.toObject());
+    try {
+      const updatedProduct = await this.productModel
+        .findOneAndUpdate({ id }, product, { new: true })
+        .exec();
+      return ProductEntity.fromObject(updatedProduct);
+    } catch (error) {
+      if (error.code === 11000)
+        throw new SkuConflictException(product.sku);
+    }
   }
 
   async deleteProduct(id: string): Promise<ProductEntity> {
     const deletedProduct = await this.productModel.findByIdAndDelete(id).exec();
-    return ProductEntity.fromObject(deletedProduct.toObject());
+    return ProductEntity.fromObject(deletedProduct);
   }
 }
