152159cba63cc77c2af5207b3b999fa0941ac4ef
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-16 15:22:01 -0400

    feat: add new api seed url env var

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -3,10 +3,12 @@ import * as Joi from 'joi';
 
 export const envs = registerAs('config', () => ({
   mongoUrl: process.env.MONGO_URI,
-  port: process.env.PORT,
+  port: parseInt(process.env.PORT, 10),
+  apiSeedUrl: process.env.API_SEED_URI,
 }));
 
 export const validateConfig = Joi.object({
-  MONGO_URI: Joi.string().required(),
+  MONGO_URI: Joi.string().uri().required(),
   PORT: Joi.number().required(),
+  API_SEED_URI: Joi.string().uri().required(),
 });
