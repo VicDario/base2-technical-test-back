26ffe1fe0da1d536bb9465176995b871eb1a7c50
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-16 13:18:45 -0400

    feat: connect http module

--- src/infrastructure/use-cases/use-cases.module.ts
+++ src/infrastructure/use-cases/use-cases.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductsUseCasesService } from './products-use-cases/products.use-cases.service';
 import { RepositoriesModule } from '@/repositories/repositories.module';
 import { CategoriesUseCasesService } from './categories-use-cases/categories.use-cases.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [RepositoriesModule],
+  imports: [RepositoriesModule, HttpModule],
   providers: [CategoriesUseCasesService, ProductsUseCasesService],
   exports: [CategoriesUseCasesService, ProductsUseCasesService],
 })
