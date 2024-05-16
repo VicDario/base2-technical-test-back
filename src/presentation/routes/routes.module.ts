fb544304685c87036311dbbbd589d0315cdad9c2
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-15 21:01:43 -0400

    feat: connect categories module

--- src/presentation/routes/routes.module.ts
+++ src/presentation/routes/routes.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { CategoriesModule } from './controllers/categories/categories.module';
 import { ProductsModule } from './controllers/products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [CategoriesModule, ProductsModule],
 })
 export class RoutesModule {}
