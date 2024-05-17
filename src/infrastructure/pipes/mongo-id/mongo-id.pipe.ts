5defa43cc8710443b4347ea67da2786352768a0d
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-16 21:29:28 -0400

    MINOR: remove unused param

--- src/infrastructure/pipes/mongo-id/mongo-id.pipe.ts
+++ src/infrastructure/pipes/mongo-id/mongo-id.pipe.ts
@@ -1,14 +1,9 @@
-import {
-  ArgumentMetadata,
-  Injectable,
-  PipeTransform,
-  BadRequestException,
-} from '@nestjs/common';
+import { Injectable, PipeTransform, BadRequestException } from '@nestjs/common';
 import { isMongoId } from 'class-validator';
 
 @Injectable()
 export class MongoIdPipe implements PipeTransform {
-  transform(value: any, _metadata: ArgumentMetadata) {
+  transform(value: any) {
     if (!isMongoId(value)) throw new BadRequestException('Invalid ID');
     return value;
   }
