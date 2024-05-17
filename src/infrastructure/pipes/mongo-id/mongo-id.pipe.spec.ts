97f60323b2100d6deb5c34474f0c420e72982831
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-16 21:32:36 -0400

    tests: mongo pipe tests

--- src/infrastructure/pipes/mongo-id/mongo-id.pipe.spec.ts
+++ src/infrastructure/pipes/mongo-id/mongo-id.pipe.spec.ts
@@ -1,7 +1,28 @@
+import { HttpException } from '@nestjs/common';
 import { MongoIdPipe } from './mongo-id.pipe';
-
+import { mongo } from 'mongoose';
 describe('MongoIdPipe', () => {
   it('should be defined', () => {
     expect(new MongoIdPipe()).toBeDefined();
+  });
+
+  it('should transform a valid id', () => {
+    const mongoId = new mongo.ObjectId().toString();
+    const pipe = new MongoIdPipe();
+
+    const result = pipe.transform(mongoId);
+
+    expect(result).toEqual(mongoId);
+  });
+
+  it('should return exception when invalid mongo id', () => {
+    const mongoId = 'fkuhas';
+    const pipe = new MongoIdPipe();
+
+    try {
+      pipe.transform(mongoId);
+    } catch (error) {
+      expect(error).toBeInstanceOf(HttpException);
+    }
   });
 });
