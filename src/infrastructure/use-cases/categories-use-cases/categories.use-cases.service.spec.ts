9260ccd3f2339540b7bdba06f18be79b61d0343d
VicDario <26585356+VicDario@users.noreply.github.com>
2024-05-17 00:27:52 -0400

    test: categories use cases test

--- src/infrastructure/use-cases/categories-use-cases/categories.use-cases.service.spec.ts
+++ src/infrastructure/use-cases/categories-use-cases/categories.use-cases.service.spec.ts
@@ -1,8 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesUseCasesService } from './categories.use-cases.service';
 import { mockCategoryRepository } from '@/infrastructure/mocks/repositories/category.repository.mock';
-import { CategoryRepository } from '@/domain/repositories/category.repository';
+import {
+  CategoriesResult,
+  CategoryRepository,
+  Pagination,
+} from '@/domain/repositories/category.repository';
 import { CategoryRepositoryService } from '@/repositories/category-repository/category.repository.service';
+import { categoriesArray } from '@/infrastructure/mocks/data/category.mock';
+import { CategoryEntity } from '@/entities/category.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('CategoriesUseCasesService', () => {
   let service: CategoriesUseCasesService;
@@ -27,5 +34,91 @@ describe('CategoriesUseCasesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getCategories', () => {
+    it('should return a list of categories', async () => {
+      const pagination: Pagination = { offset: 0, limit: 10 };
+      const mockedResult: CategoriesResult = {
+        categories: categoriesArray
+          .filter((_, index) => index < pagination.limit)
+          .map(CategoryEntity.fromObject),
+        total: categoriesArray.length,
+      };
+      jest
+        .spyOn(categoryRepository, 'getCategories')
+        .mockResolvedValue(mockedResult);
+
+      const result = await service.getCategories(pagination);
+
+      expect(result).toBeDefined();
+      expect(result).toMatchObject(mockedResult);
+      result.categories.forEach((category) =>
+        expect(category).toBeInstanceOf(CategoryEntity),
+      );
+      expect(result.categories).toHaveLength(categoriesArray.length);
+    });
+  });
+
+  describe('getCategoryById', () => {
+    it('should return a category by id', async () => {
+      const id = '1';
+      const category = CategoryEntity.fromObject(categoriesArray[0]);
+      jest
+        .spyOn(categoryRepository, 'getCategoryById')
+        .mockResolvedValue(category);
+
+      const result = await service.getCategoryById(id);
+
+      expect(result).toBeDefined();
+      expect(result).toBeInstanceOf(CategoryEntity);
+    });
+    it('should throw an error if category is not found', async () => {
+      const id = 'invalid-id';
+      jest.spyOn(categoryRepository, 'getCategoryById').mockResolvedValue(null);
+
+      await expect(service.getCategoryById(id)).rejects.toThrow();
+      await expect(service.getCategoryById(id)).rejects.toBeInstanceOf(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('createCategory', () => {
+    it('should create a category', async () => {
+      const category = CategoryEntity.fromObject(categoriesArray[0]);
+      jest
+        .spyOn(categoryRepository, 'createCategory')
+        .mockResolvedValue(category);
+
+      const result = await service.createCategory(category);
+
+      expect(result).toBeDefined();
+      expect(result).toBeInstanceOf(CategoryEntity);
+    });
+  });
+
+  describe('deleteCategory', () => {
+    it('should delete a category', async () => {
+      const id = '1';
+      const category = CategoryEntity.fromObject(categoriesArray[0]);
+      jest
+        .spyOn(categoryRepository, 'deleteCategory')
+        .mockResolvedValue(category);
+
+      const result = await service.deleteCategory(id);
+
+      expect(result).toBeDefined();
+      expect(result).toBeInstanceOf(CategoryEntity);
+    });
+    it('should throw an error if category is not found', async () => {
+      const id = 'invalid-id';
+      jest.spyOn(categoryRepository, 'deleteCategory').mockResolvedValue(null);
+
+      await expect(service.deleteCategory(id)).rejects.toThrow();
+      await expect(service.deleteCategory(id)).rejects.toBeInstanceOf(
+        NotFoundException,
+      );
+    });
   });
 });
